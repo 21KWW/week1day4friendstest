7568e8ae4f500f3a881c18ac21994295b8eb9547
Keith Wyse <kwyse3436@gmail.com>
2021-10-21 12:13:30 +0100

    Third function

--- src/friends.py
+++ src/friends.py
@@ -3,3 +3,11 @@ def get_name(person):
 
 def get_favourite_tv_show(person):
     return person["favourites"]["tv_show"]
+
+def likes_to_eat(person, snack):
+    for food in person["favourites"]["snacks"]:
+        if snack == food:
+            return True
+    
+    return False
+
